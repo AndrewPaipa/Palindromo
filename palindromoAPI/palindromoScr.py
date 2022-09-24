af00c8abb7ec346bbe92ae6579b2ab30efc5e662
Andrew Paipa <andreuu12345@gmail.com>
2022-09-24 08:40:42 -0500

    Comentarios logica Palindromo

--- palindromoAPI/palindromoScr.py
+++ palindromoAPI/palindromoScr.py
@@ -2,16 +2,16 @@
 
 def funtionPalindromo(expression):
 
-    longExpression = len(expression)
+    longExpression = len(expression)                    #longitud de la cadena entrante
     
-    for i in range(1,longExpression):
+    for i in range(1,longExpression):                   #bucle de 1 a longitud - 1  // empueza por la subcadena más grande "todo el str"
 
-        for j in range(0,i):
+        for j in range(0,i):                            #barrido de subcadenas en la cadena 
 
-            invested = (longExpression + 1) - i
-            subStr = expression[j:(j+invested)]
+            invested = (longExpression + 1) - i         #calculo de repeticione de subcadena
+            subStr = expression[j:(j+invested)]         #Sub cadena 
 
-            if subStr == subStr[::-1]:
+            if subStr == subStr[::-1]:                  #Comprobando palindromo  
 
                 return(subStr)
     
