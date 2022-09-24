def0e403378ffc6cbc3b0e8cd7022a507b593684
Andrew Paipa <andreuu12345@gmail.com>
2022-09-24 03:16:17 -0500

    create view.Palandromo GET

--- palindromoAPI/views.py
+++ palindromoAPI/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.forms.models import model_to_dict
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.views import View
+
 
 # Create your views here.
+class CompanyListView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        # get...
+        clist = {'message':'success'}
+        return JsonResponse(list(clist)
