43834da4ae32cfa8d594947c080a10cf0bea13b5
Andrew Paipa <andreuu12345@gmail.com>
2022-09-26 21:43:20 -0500

    Actualization

--- palindromoAPI/palindromoScr.py
+++ palindromoAPI/palindromoScr.py
@@ -3,15 +3,23 @@
 def funtionPalindromo(expression):
 
     longExpression = len(expression)                    #longitud de la cadena entrante
+    subStrVector = []
     
     for i in range(1,longExpression):                   #bucle de 1 a longitud - 1  // empueza por la subcadena más grande "todo el str"
 
+        invested = (longExpression + 1) - i             #calculo de repeticione de subcadena
+
         for j in range(0,i):                            #barrido de subcadenas en la cadena 
 
-            invested = (longExpression + 1) - i         #calculo de repeticione de subcadena
+            
             subStr = expression[j:(j+invested)]         #Sub cadena 
+
 
             if subStr == subStr[::-1]:                  #Comprobando palindromo  
 
-                return(subStr)
-    
+                subStrVector.append(subStr)             #Vector para validar si hay más de un solo palindromo del tamaño más largo
+        
+    if subStrVector == []:                              #si el vector esta vacio o no
+        return(None)
+    else:
+        return(subStrVector)
